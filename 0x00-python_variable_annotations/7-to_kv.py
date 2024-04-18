80c231fe795fad11f80b1f875ca8a9ddf83d62e9
Ayomide Kayode <ayomidekay7@gmail.com>
2024-04-18 16:01:22 +0100

    Complex types - functions...🚧

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -12,6 +12,7 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Function to return a tuple with a string and the square of an int or float.
+    """Function to return a tuple with a string
+        and the square of an int or float.
     """
     return (k, v ** 2)
