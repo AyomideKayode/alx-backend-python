5305884cbf18038a02c9ae133ec9ca55a41702c6
Ayomide Kayode <ayomidekay7@gmail.com>
2024-04-22 16:02:40 +0100

    Refactor task_wait_n function to use asyncio.as_completed...🚧

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -20,5 +20,5 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
         list of delays
     """
-    tasks: List[Task] = [task_wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
